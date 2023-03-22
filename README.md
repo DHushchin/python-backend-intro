cdf2a1c7ea927f2f2fd5dce75a63535d8fb795dd
Ervin <70893070+Er4ik@users.noreply.github.com>
2023-03-23 01:03:19 +0200

    fix: fixed readme and .env.example

--- README.md
+++ README.md
@@ -5,6 +5,10 @@
 git clone https://github.com/DHushchin/django-intro
 ```
 
+## Usage
+
+- Go to the necessary directory lab
+
 ### Create virtual environment
 ```bash
 python -m venv venv
@@ -19,4 +23,14 @@ source venv/bin/activate (MacOS)
 ### Install dependencies
 ```bash
 pip install -r requirements.txt
+```
+
+### Create .env
+- create **.env** file in lab directory
+- copy data from **.env.example** file to **.env**
+- fill empty fields with your credentials
+
+### Running
+```bash
+python ./src/menu.py
 ```
--- lab1/src/config.py
+++ lab1/src/config.py
@@ -1,14 +1,18 @@
 import dotenv
 import os
 
+# Load environment variables from .env file
+
 dotenv.load_dotenv()
 
+# Define a configuration class
 class Config:
     def __init__(self):
         self.mysql_config()
         self.postgres_config()
         self.sqlite_config()
     
+    # Define MySQL configurations
     def mysql_config(self):
         self.MYSQL_HOST = os.environ.get('MYSQL_HOST')
         self.MYSQL_USER = os.environ.get('MYSQL_USER')
@@ -16,6 +20,7 @@ class Config:
         self.MYSQL_DATABASE = os.environ.get('MYSQL_DATABASE')
         self.MYSQL_PORT = os.environ.get('MYSQL_PORT')
         
+    # Define PostgreSQL configurations
     def postgres_config(self):
         self.POSTGRES_HOST = os.environ.get('POSTGRES_HOST')
         self.POSTGRES_USER = os.environ.get('POSTGRES_USER')
@@ -23,5 +28,6 @@ class Config:
         self.POSTGRES_DATABASE = os.environ.get('POSTGRES_DATABASE')
         self.POSTGRES_PORT = os.environ.get('POSTGRES_PORT')
         
+    # Define SQLite configurations
     def sqlite_config(self):
         self.SQLITE_DATABASE = os.environ.get('SQLITE_DATABASE')
